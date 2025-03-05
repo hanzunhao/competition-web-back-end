971d3249e9b558a931bb308da8234c334081e766
hzh <hanzunhao2004@163.com>
2025-03-05 22:36:36 +0800

    完善了登录功能

--- src/main/java/cn/edu/usst/competitionweb/filter/LoginCheckFilter.java
+++ src/main/java/cn/edu/usst/competitionweb/filter/LoginCheckFilter.java
@@ -1,4 +1,4 @@
-package cn.edu.usst.competitionweb.pojo.filter;
+package cn.edu.usst.competitionweb.filter;
 
 import cn.edu.usst.competitionweb.pojo.Result;
 import cn.edu.usst.competitionweb.utils.JwtUtils;
@@ -19,37 +19,42 @@ public class LoginCheckFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) servletRequest;
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
 
-        //1.获取请求url
+        // 检查是否是 WebSocket 握手请求
+        if ("Upgrade".equalsIgnoreCase(req.getHeader("Connection")) && "websocket".equalsIgnoreCase(req.getHeader("Upgrade"))) {
+            // WebSocket 握手请求，直接放行
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+
+        // 其他 HTTP 请求的逻辑
         String url = req.getRequestURL().toString();
         log.info("请求的url：{}", url);
-        //2.判断url中是否含有login，如果包含，放行
         if (url.contains("login")) {
             filterChain.doFilter(servletRequest, servletResponse);
             return;
         }
-        //3.获取请求头中的令牌token
+
         String jwt = req.getHeader("token");
         log.info(jwt);
-        //4.判断令牌是否存在，如果不存在，返回错误结果（未登录）
         if (!StringUtils.hasLength(jwt)) {
             log.info("请求头token为空，返回未登陆的信息");
             Result error = Result.error("NOT_LOGIN");
-            String notLogin = JSONObject.toJSONString(error);//由于并不在controller层，需要手动将Result转为json（阿里巴巴fastJSON工具包）
-            resp.getWriter().write(notLogin);//通过流直接返回给浏览器
+            String notLogin = JSONObject.toJSONString(error);
+            resp.getWriter().write(notLogin);
             return;
         }
-        //5.解析token，如果解析失败，返回错误结果（未登录）
+
         try {
             JwtUtils.parseJWT(jwt);
         } catch (Exception e) {
             e.printStackTrace();
             log.info("解析令牌失败返回未登录的错误信息");
             Result error = Result.error("NOT_LOGIN");
-            String notLogin = JSONObject.toJSONString(error);//由于并不在controller层，需要手动将Result转为json（阿里巴巴fastJSON工具包）
-            resp.getWriter().write(notLogin);//通过流直接返回给浏览器
+            String notLogin = JSONObject.toJSONString(error);
+            resp.getWriter().write(notLogin);
             return;
         }
-        //6.放行
+
         log.info("令牌合法，直接放行");
         filterChain.doFilter(servletRequest, servletResponse);
     }
