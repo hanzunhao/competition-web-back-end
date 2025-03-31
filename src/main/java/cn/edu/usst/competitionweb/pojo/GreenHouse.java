4a8ca2d2f7e8812bcb5e9ee9c0645de9af6ac11e
hzh <hanzunhao2004@163.com>
2025-03-31 20:16:26 +0800

    实现了花盆和温室的管理接口

--- src/main/java/cn/edu/usst/competitionweb/pojo/GreenHouse.java
+++ src/main/java/cn/edu/usst/competitionweb/pojo/GreenHouse.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
 
 /**
  * 温室（大棚）信息类
@@ -14,15 +13,15 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class GreenHouse {
-    private Integer GreenHouseId;
+    private Integer greenHouseId;
+    private Integer storeId;
     private String flowerName;
+    private Integer totalPots;
+    private Integer potsWithFlowers;
     private Double illumination;
     private Double airTemperature;
     private Double meanSoilTemperature;
     private Double airHumidity;
     private Double meanSoilHumidity;
-    private List<String> pestNames;
-    private Integer totalPots;
-    private Integer potsWithFlowers;
-    private Integer storeId;
+    private String pestNames;
 }
